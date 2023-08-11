5b2a4ffc8097fd73deb913128a97385632ce4dc3
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-08-11 22:07:56 +0300

    C# abstraction example
    - Tinh truu tuong

--- CsPractice/Program.cs
+++ CsPractice/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace CsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Laptop l = new Laptop();
+            l.Brand = "Dell";
+            l.Model = "Inspiron 14R";
+            l.LaptopDetails();
+            Console.WriteLine("\nPress Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}
