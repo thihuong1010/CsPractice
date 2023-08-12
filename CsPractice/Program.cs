44ba9e574e9108087c410999c96131f2f7cd60b4
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-08-12 22:43:27 +0300

    C# encapsulation example

--- CsPractice/Program.cs
+++ CsPractice/Program.cs
@@ -2,14 +2,33 @@
 
 namespace CsPractice
 {
+    // C# abstraction
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Laptop l = new Laptop();
+    //        l.Brand = "Dell";
+    //        l.Model = "Inspiron 14R";
+    //        l.LaptopDetails();
+    //        Console.WriteLine("\nPress Enter Key to Exit..");
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // C# encapsulation
     class Program
     {
         static void Main(string[] args)
         {
-            Laptop l = new Laptop();
-            l.Brand = "Dell";
-            l.Model = "Inspiron 14R";
-            l.LaptopDetails();
+            User u = new User();
+            // set accessor will invoke
+            u.Name = "Huong Nguyen";
+            u.Location = "Finland";
+            // get accessor will invoke
+            Console.WriteLine("Name: " + u.Name);
+            Console.WriteLine("Location: " + u.Location);
+
             Console.WriteLine("\nPress Enter Key to Exit..");
             Console.ReadLine();
         }
---
+++ CsPractice/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace CsPractice
+{
+	public class User
+	{
+		private string? location;
+		private string? name;
+
+		public string Location
+		{
+			get
+			{
+				return location;
+			}
+			set
+			{
+				location = value;
+			}
+		}
+		public string Name
+		{
+			get
+			{
+				return name;
+			}
+			set
+			{
+				name = value;
+			}
+		}
+	}
+}
+
