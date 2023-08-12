a35104f8337fc8a4da9929a8fae659ab1643667d
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-08-12 23:16:40 +0300

    C# inheritance example

--- CsPractice/Program.cs
+++ CsPractice/Program.cs
@@ -17,19 +17,37 @@ namespace CsPractice
     //}
 
     // C# encapsulation
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        User u = new User();
+    //        // set accessor will invoke
+    //        u.Name = "Huong Nguyen";
+    //        u.Location = "Finland";
+    //        // get accessor will invoke
+    //        Console.WriteLine("Name: " + u.Name);
+    //        Console.WriteLine("Location: " + u.Location);
+
+    //        Console.WriteLine("\nPress Enter Key to Exit..");
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // C# inheritance
     class Program
     {
         static void Main(string[] args)
         {
-            User u = new User();
-            // set accessor will invoke
-            u.Name = "Huong Nguyen";
-            u.Location = "Finland";
-            // get accessor will invoke
-            Console.WriteLine("Name: " + u.Name);
-            Console.WriteLine("Location: " + u.Location);
+            Detail d = new Detail();
+            d.Name = "Huong Nguyen";
+            // Compile time error because Location is private
+            // d.Location = "Finland";
+            d.Age = 23;
+            d.GetUserInfo("Finland");
+            d.GetAge();
 
-            Console.WriteLine("\nPress Enter Key to Exit..");
+            Console.WriteLine("\nPress Any Key to Exit..");
             Console.ReadLine();
         }
     }
---
+++ CsPractice/UserInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace CsPractice
+{
+	public class UserInfo
+	{
+		public string? Name;
+		private string? Location;
+		public UserInfo()
+		{
+			Console.WriteLine("Base class contructor");
+		}
+		public void GetUserInfo(string location)
+		{
+			Location = location;
+			Console.WriteLine("Name: {0}", Name);
+			Console.WriteLine("Location: {0}", Location);
+		}
+	}
+	public class Detail: UserInfo
+	{
+		public int Age;
+		public Detail()
+		{
+			Console.WriteLine("Child class constructor");
+		}
+		public void GetAge()
+		{
+			Console.WriteLine("Age: {0}", Age);
+		}
+	}
+}
+
